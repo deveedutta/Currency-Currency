26d84676b78b535dbce8674290ee834fe269728c
DeveeDutta.Maharana@snapdeal.com <deveedutta.maharana@L-5ZP44Z1.jasperindia.local>
2014-12-14 02:36:38 +0530

    second commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+void function (D) {
+	var
+		window		= this || (0, eval)('this')	
+		//source	: http://stackoverflow.com/questions/14119988/return-this-0-evalthis/14120023#14120023
+		//courtesy	: knockout-js
+	,	undefined	= void 0
+	,	dictionary	= null				//dictionary is a marker saying upcoming VARs are short-hands
+	,	_AC			= "appendChild"
+	,	CEL			= "createElement"
+	,	QSA			= "querySelectorAll"
+	,	GID			= "getElementById"
+	,	_IH			= "innerHTML"
+	
+	;
+	
+	var s = D[CEL]("s");
+	// s.src = "//code.jquery.com/jquery-2.1.1.js";			//production
+	s.src = "bower_components/jquery/dist/jquery.min.js";	//dev
+	D["body"][_AC](s);
+	s.onload = function () {
+		$(function () {
+			alert($ instanceof jQuery);
+		});
+	};
+	
+}(document);
+
+$.getJSON( "ajax/test.json", function(data ) {
+  var items = [];
+  $.each( data, function( key, val ) {
+    items.push( "<li id='" + key + "'>" + val + "</li>" );
+  });
+ 
+  $( "<ul/>", {
+    "class": "my-new-list",
+    html: items.join( "" )
+  }).appendTo( "body" );
+});
