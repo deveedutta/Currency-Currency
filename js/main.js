45e98fc0ca25f815c942966cd6341476306a9ecc
DeveeDutta.Maharana@snapdeal.com <deveedutta.maharana@L-5ZP44Z1.jasperindia.local>
2014-12-14 09:26:05 +0530

    sarvanaaash

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-window._currencies	= [];
-window._rates		= [];
-
 //Support : Chrome 13+ Firefox 6.0+ Internet Explorer 5.5+ Opera 11.60+ Safari 5.1+ 
 window.onerror = function globalErrorListener() {
 	var args = [].slice.call(arguments);
@@ -9,6 +6,9 @@ window.onerror = function globalErrorListener() {
 };
 
 $(function () {
+	window._currencies		= [];
+	window._otherCurrencies	= [];
+	window._rates			= [];
 	// $.getJSON( "https://gist.githubusercontent.com/Fluidbyte/2973986/raw/9ead0f85b6ee6071d018564fa5a314a0297212cc/Common-Currency.json",
 	$.getJSON( "js/currency-list.js",
 	function(data ) {
@@ -20,8 +20,10 @@ $(function () {
 	// $.getJSON( "https://openexchangerates.org/api/latest.json?app_id=f9009aea3ff84eceac990e86e968ce9d",
 	$.getJSON( "js/latest.json",
 	function(data) {
-		for ( var name in data['rates'] ) {
-			_rates.push(name);
+		data = data['rates'];
+		for ( var key in data) {
+			_otherCurrencies.push( key );
+			_rates.push(data[key]);
 		}
 	});
 	$parentContainer = $("#parentContainer");
@@ -33,33 +35,18 @@ $(function () {
 		select: function( event, ui ) {
 			var url = "http://openexchangerates.org/api/latest.json?app_id=f9009aea3ff84eceac990e86e968ce9d";
 			$("#currencyType").val($(this).val());
-			$("#currencyAmount");
-			getRes ( url );
 		}
 	});
 	$( "#comparisonCurrency" ).autocomplete({
 		source: _currencies,
 		select: function( event, ui ) {
-				
+			var val = $(this).val();
+			var idx = _otherCurrencies.indexOf(val);
+			$("#displayTable").append("<tr><td>" + _rates[idx] *   + "<td>" t+  + "</td>");
 		}
 	});
 	
-	function getRes ( URL ) {
-		$.getJSON( URL, function(data ) {
-			var items = data['rates'];
-			$.each( data, function( key, val ) {
-				items.push( "<tr> <td>" + key + "</td><td>" + val + "</td> </tr>" );
-			});
-			$("#displayTable").empty().html(items.join(""));
-		})
-		.done(function() {
-			console.log( "second success" );
-		})
-		.fail(function() {
-			console.log( "error" );
-		})
-		.always(function() {
-			console.log( "complete" );
-		});
+	function getRes ( ) {
+		
 	}
 });
