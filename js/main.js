83d9a6e270830aea4cce16031612fa2cd2f0bde2
DeveeDutta.Maharana@snapdeal.com <deveedutta.maharana@L-5ZP44Z1.jasperindia.local>
2014-12-14 10:09:54 +0530

    sarvanaaash II

--- js/main.js
+++ js/main.js
@@ -7,8 +7,14 @@ window.onerror = function globalErrorListener() {
 
 $(function () {
 	window._currencies		= [];
-	window._otherCurrencies	= [];
-	window._rates			= [];
+	window._ratesWRTUSD		= [];
+	var _listedCurrencies	= [];
+	
+	var _baseCurrencyName 	= "";
+	var _baseCurrencyValue 	= 1;
+	
+	var _otherCurrenciesSelected 	= [];
+	
 	// $.getJSON( "https://gist.githubusercontent.com/Fluidbyte/2973986/raw/9ead0f85b6ee6071d018564fa5a314a0297212cc/Common-Currency.json",
 	$.getJSON( "js/currency-list.js",
 	function(data ) {
@@ -17,36 +23,57 @@ $(function () {
 		}
 	});
 	
+	
 	// $.getJSON( "https://openexchangerates.org/api/latest.json?app_id=f9009aea3ff84eceac990e86e968ce9d",
 	$.getJSON( "js/latest.json",
 	function(data) {
 		data = data['rates'];
 		for ( var key in data) {
-			_otherCurrencies.push( key );
-			_rates.push(data[key]);
+			_otherCurrenciesSelected.push( key );
+			_ratesWRTUSD.push(data[key]);
 		}
 	});
-	$parentContainer = $("#parentContainer");
-	_items 	 = $("[data-observable]", $parentContainer);
 	
+
+	
+	$("#currencyAmount").on("change", function () {
+		_baseCurrencyName = $("#currencyType").val();
+		_baseCurrencyValue = $(this).val() * 1;
+		drawTable();
+	});
 	
 	$( "#baseCurrencyType" ).autocomplete({
 		source: _currencies,
 		select: function( event, ui ) {
-			var url = "http://openexchangerates.org/api/latest.json?app_id=f9009aea3ff84eceac990e86e968ce9d";
-			$("#currencyType").val($(this).val());
+			var val = $(this).val();
+			_baseCurrencyName = val;
+			$("#currencyType").val(val);
+			drawTable();
 		}
 	});
 	$( "#comparisonCurrency" ).autocomplete({
 		source: _currencies,
 		select: function( event, ui ) {
 			var val = $(this).val();
-			var idx = _otherCurrencies.indexOf(val);
-			$("#displayTable").append("<tr><td>" + _rates[idx] *   + "<td>" t+  + "</td>");
+			var idx = _otherCurrenciesSelected.indexOf(val);
+			_listedCurrencies.push(val);
+			
+			drawTable();
+			
 		}
 	});
 	
-	function getRes ( ) {
+	function drawTable( ) {
+		$("#displayTable").empty();
+		_baseCurrencyValue = ($("#currencyAmount").val() * 1);	//integer conversion hack
+		
+		for ( var i=0; i<_listedCurrencies.length; i++) {
+			var idx = _otherCurrenciesSelected.indexOf(_listedCurrencies[i]);
+			if(_baseCurrencyName != _listedCurrencies[i])
+				$("#displayTable").append("<tr><td>" + _ratesWRTUSD[idx] * _baseCurrencyValue  + "<td>" + _listedCurrencies[i] + "</td>") ;
+			else
+				$("#displayTable").append("<tr><td>" + _baseCurrencyValue  + "<td>" + _listedCurrencies[i] + "</td>") ;
+		}	
 		
 	}
 });
